1f6b62743eea23982fd4f1c96385c5d8cee46893
Shanmathi Thamizhinian <122631445+Shanmathi-Thamizhinian@users.noreply.github.com>
2024-01-17 16:41:18 +0530

    Create d1_q5.cpp

---
+++ d1_q5.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n=prices.size();
+        int i=0;
+        int j=0;
+        int buy,sell,profit=0;
+        while(i<n-1){
+            while(i<n-1 && prices[i]>=prices[i+1]) i++;
+            buy=prices[i];
+            while(i<n-1 && prices[i]<prices[i+1]) i++;
+            sell=prices[i];
+            profit=profit+(sell-buy);
+        }
+        return profit;
+    }
+};
