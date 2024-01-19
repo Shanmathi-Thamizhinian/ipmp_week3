2fab6fc2c9ce87697f08238ab6032f1ce9c3e38d
Shanmathi Thamizhinian <122631445+Shanmathi-Thamizhinian@users.noreply.github.com>
2024-01-19 06:48:09 +0530

    Create d3_q1.cpp

---
+++ d3_q1.cpp
@@ -0,0 +1,46 @@
+//{ Driver Code Starts
+#include<bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+
+class Solution{
+    public:
+    int maxOnes(int a[], int n)
+    {
+        int count = 0;
+        int maxdiff = 0;
+        int iter = 0;
+        for(int i=0; i<n; i++)
+        {
+            count += a[i];
+            iter += (a[i]==0 ? 1 : -1);
+            
+            if(iter > maxdiff) maxdiff = iter;
+            else if(iter < 0) iter = 0;
+            else ;
+        }
+        
+        return count + maxdiff;
+
+    }
+};
+
+//{ Driver Code Starts.
+int main()
+{
+    int t; cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        int a[n+5];
+        for(int i=0;i<n;i++)
+            cin>>a[i];
+        Solution ob;
+        cout<< ob.maxOnes(a, n) <<endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
